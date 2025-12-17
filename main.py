8da4bab2240cac5c9341c1fcd8a587eb02fedb2c
bragaemprego262-commits <bragaemprego262@gmail.com>
2025-12-17 14:38:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def verificar():
     return {
         "status": "PENDENTE",
         "mensagem": "O cliente possui pendências financeiras ativas e necessita regularização.",
-        "valor": 1650.00,
-        "vencimento": "2023-10-15"
+        "valor": 1670.00,
+        "vencimento": "2025-10-15"
     }
 
 @app.get("/meiospagamento")
@@ -20,14 +20,14 @@ def verificar():
     return [
         {
         "meio": "PIX",
-        "endereco": "O cliente possui pendências financeiras ativas e necessita regularização.",
-        "valor": 1650.00,
-        "vencimento": "2023-10-15"
+        "código para pagamento pix": "00020126580014br.gov.bcb.pix0136a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z600170014br.gov.bcb.pix0210pagador@email.com5204000053039865802BR5912Nome",
+        "valor": 1680.00,
+        "vencimento": "2025-12-30"
         },
         {
         "meio": "BOLETO",
-        "endereco": "O cliente possui pendências financeiras ativas e necessita regularização.",
-        "valor": 1613.00,
-        "vencimento": "2023-10-10"
+        "link de pagamento do boleto": "banco.bradesco.com",
+        "valor": 1672.00,
+        "vencimento": "2026-01-10"
         }
     ]
